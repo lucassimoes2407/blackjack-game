6f7b5d4749a7870df2ded15a7f883c3ac2d3eda8
Perkles <lucassimoes871@hotmail.com>
2022-01-27 22:01:09 -0300

    BlackJack game using javascript

---
+++ index2.js
@@ -0,0 +1,68 @@
+
+let cards=[]
+let sum=0
+let hasBlackJack=false
+let isAlive=false
+let message=""
+let messageEL=document.getElementById("message-el")
+let sumEL=document.getElementById("sum-el")
+let cardsEL=document.getElementById("card-el")
+let player ={
+    name:"Lucas",
+    chips: 145,
+
+}
+
+
+let playerEl= document.getElementById("player-el")
+playerEl.textContent=player.name+": R$ "+ player.chips
+function getRandomCard(){
+    let randomNumber=Math.floor(Math.random()*13)+1
+    if(randomNumber>10){
+        return 10
+    } else if(randomNumber ===1){
+        return 11
+    }
+    else return randomNumber
+}
+
+function startGame(){
+    isAlive=true
+    let firstCard=getRandomCard()
+    let secondCard=getRandomCard()
+    cards=[firstCard,secondCard]
+    sum=firstCard+secondCard
+    renderGame()
+    }
+function renderGame ()
+{
+    cardsEL.textContent="Cards: "
+    for(let i=0;i<cards.length;i++) {
+        cardsEL.textContent += cards[i] + " "
+    }
+
+    sumEL.textContent="Your Sum: " + sum
+
+    if (sum<=20){
+        message="Do you want to draw a new card?"
+    }
+    else if(sum===21){
+        message="You won the blackjack game!"
+        hasBlackJack=true
+    }
+    else {
+        message="You are out of the game"
+        isAlive= false
+    }
+messageEL.textContent=message
+}
+
+function newCard() {
+    if(isAlive === true && hasBlackJack===false){
+    let thirdcard = getRandomCard()
+    sum += thirdcard
+    cards.push(thirdcard)
+
+    renderGame()
+}
+}
